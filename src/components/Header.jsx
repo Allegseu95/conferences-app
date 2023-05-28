2393a53b9f2c0883fe8b339fb37ceb47f16261ba
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-05-28 18:15:25 -0500

    feat(componenet): ✨ add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div className='row mb-4'>
+      <div className='col-sm-12 d-flex justify-content-center p-3 bg-dark w-100 text-light text-center'>
+        <h2>2DO. Congreso Internacional de Medicina</h2>
+      </div>
+    </div>
+  );
+};
