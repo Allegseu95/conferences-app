30674a296b6268594c127fa9bac585f785c40fd4
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-05-28 18:17:56 -0500

    feat(page): ✨ add login page

--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { HomePage } from '@/pages/public/Home';
+import { LoginPage } from '@/pages/public/Login';
 
 export const PublicRoutes = () => (
   <Routes>
     <Route path='/' element={<HomePage />} />
+    <Route path='/login' element={<LoginPage />} />
     <Route path='*' element={<Navigate to='/' />} />
   </Routes>
 );
