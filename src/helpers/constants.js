f8dc7864d6b4c8d700feee3d8aec26b7c6e5d1eb
Rommel-Palma <rommelpalma.3000@gmail.com>
2023-05-30 20:56:12 -0500

    fix: :building_construction: actualizacion de componentes y pantallas

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -32,3 +32,16 @@ export const initFormLogin = {
   email: '',
   password: '',
 };
+
+export const updateProfile = [
+  {
+    id: '1',
+    name: 'rommel santiago',
+    lastname: 'palma plua',
+    email: 'ejemplo@gmail.com',
+    phone: '0967764587',
+    cedula: '230053614',
+    adress: 'manta-ecuador',
+    company: 'universidad laica elou alfaro de manabi'
+  }
+];
