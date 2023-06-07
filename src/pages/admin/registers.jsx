37732281292940d856e0829015e3be134b96ab36
gustavo reyes <gustavino182@gmail.com>
2023-06-06 21:40:46 -0500

    Cambios en aceptacion y eliminacion de registros y cambios en estilos de la tabla

--- src/pages/admin/registers.jsx
+++ src/pages/admin/registers.jsx
@@ -1,41 +1,75 @@
 import React, { useState, useEffect } from 'react';
-import momentjs from 'moment';
-
+import momentjs from 'moment'
 import '@/static/base/base.css';
-import { faXmark, faEye, faCheck, faFolder } from '@fortawesome/free-solid-svg-icons';
+import {
+  faUser,
+  faScrewdriver,
+  faBullseye,
+  faXmark,
+  faEye,
+  faCheck,
+  faChainBroken,
+  faFolder,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { showBasicAlert, question, see } from '@/helpers/sweetAlert';
+import { showBasicAlert,question, see } from '@/helpers/sweetAlert';
 
 import { Sidebar } from '@/pages/admin/sidebar';
 import { useServer } from '@/contexts/ServerContext';
 
 export const Regitsers = ({ data, itemsPerPage }) => {
-  const server = useServer();
 
-  const [regsters, setregisters] = useState([]);
+
+  const useserverapi = useServer();
+
+  const [regsters, setregisters] = useState([])
   const [currentPage, setCurrentPage] = useState(1);
   const totalPages = Math.ceil(data.length / itemsPerPage);
 
-  const getregisterapi = async () => {
-    const datos = await server.getRegisters();
-    setregisters(datos);
+  const getregisterapi = async ()=>{
+    const datos = await useserverapi.getRegisters();
+    setregisters(datos)
   };
 
-  const updateregister = async (status, registerid) => {
-    const datosupdate = Object.freeze({
-      status,
-      registerId: registerid,
-    });
-    const datosregistro = await server.UpdateStatus(datosupdate);
-    if (datosregistro !== undefined) {
-      confirmar('Registro actualizado con exito', 'registro actualizado');
+  const updateregister= async(status, registerid)=>{
+
+    let sendstatus = '';
+    if (status === "pending") {
+      sendstatus = "paid";
     }
-  };
 
-  useEffect(() => {
+    const datosupdate = Object.freeze({
+      status:sendstatus, registerId:registerid
+    })
+    const datosregistro = await useserverapi.UpdateStatus(datosupdate);
+    if (datosregistro !== undefined) {
+      confirmar('Registro actualizado con exito', 'registro actualizado')
+    }
+    await getregisterapi();
+
+  }
+  const cancelregister= async(status, registerid)=>{
+
+    let sendstatus = '';
+    if (status === "paid") {
+      sendstatus = "pending";
+    }
+
+    const datosupdate = Object.freeze({
+      status:sendstatus, registerId:registerid
+    })
+    const datosregistro = await useserverapi.UpdateStatus(datosupdate);
+    if (datosregistro !== undefined) {
+      confirmar('Registro Cancelado con exito', 'registro actualizado')
+    }
+    await getregisterapi();
+
+  }
+
+  useEffect(()=>{
     getregisterapi();
-  }, []);
+  }, [])
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -44,9 +78,15 @@ export const Regitsers = ({ data, itemsPerPage }) => {
   const confirmar = (titulo, accion) => {
     showBasicAlert(titulo, 'success', accion);
   };
-  const ver = (url) => {
-    see('Verificación de voucher', `${url}`, '200px', '200px');
-  };
+  const ver = (url => {
+    see(
+      'Verificación de voucher',
+      // 'https://www.ejemplode.com/images/uploads/voucher.jpg',
+      `${url}`,
+      '200px',
+      '200px'
+    );
+  });
   const eliminar = () => {
     question(
       'Esta seguro de rechazar el registro',
@@ -55,46 +95,68 @@ export const Regitsers = ({ data, itemsPerPage }) => {
     );
   };
 
-  const getInscriptions = (array) => {
-    const inscriptions = [];
-    array.forEach((element) => {
-      let pushed = element.courseId === null ? '' : element.courseId.description;
+  const getInscriptionstitle =(array)=>{
+    const inscriptions = []
+    array.forEach(element => {
+      let pushed = element.courseId  === null? '':element.courseId.title; 
       inscriptions.push(pushed);
     });
     return inscriptions.toString();
-  };
+  }
 
   const renderTableData = () => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const slicedRegsters = regsters.slice(startIndex, endIndex);
-
     return slicedRegsters.map((row, index) => {
-      const { _id, typePayment, total, status, voucherURL, createdAt, inscriptions, userId } = row;
+      const { 
+        _id, typePayment, total, status, voucherURL,
+        createdAt, inscriptions, userId
+       } = row;
       return (
         <tr key={index}>
-          <td className='padding_td'>{typePayment}</td>
-          <td className='padding_td'>{momentjs(createdAt).format('YYYY-MM-DD')}</td>
-          <td className='padding_td'>{getInscriptions(inscriptions)}</td>
+          
+
           <td className='padding_td'>
-            {userId === null ? 'Sin usuario' : userId.name + ' ' + userId.lastname}
+             {
+              typePayment == 'transfer'?
+              'transferencia'
+              : typePayment    == 'efective'?
+              'Efectivo'
+              :''
+              }
           </td>
-          <td className='padding_td'>{total}</td>
-          <td className='padding_td'>{status}</td>
+          {/* <td className='padding_td'>{ momentjs(createdAt).format('YYYY-MM-DD')}</td> */}
+          <td className='padding_td'>{ momentjs(createdAt).format('DD-MM-YYYY')}</td>
+          <td className='padding_td'>{getInscriptionstitle(inscriptions)}</td>
+          <td className='padding_td'>{userId === null? 'Sin usuario': userId.name + ' ' + userId.lastname}</td>
+          <td className='padding_td'>{total +'$'}</td>
+          <td className='padding_td'>
+            {
+          status == 'paid'?
+          'pagado'
+          : status == 'pending'?
+          'pendiente'
+          :''
+          }
+          </td>
           <td className='padding_td options'>
-            {/* <button onClick={getregisterapi()} className='btn btn-success p-1'>
+          {/* <button onClick={getregisterapi()} className='btn btn-success p-1'>
           </button> */}
-            <button onClick={() => updateregister(status, _id)} className='btn btn-success p-1'>
+            <button onClick={()=> updateregister(status, _id)} className='btn btn-success p-1 left'>
               <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
             </button>
-            <button onClick={eliminar} className='btn btn-danger p-1'>
+            <button onClick={()=> cancelregister(status, _id)} className='btn btn-danger p-1 left'>
               <FontAwesomeIcon icon={faXmark}></FontAwesomeIcon>
             </button>
             <button
-              onClick={() => ver(voucherURL)}
-              className='btn btn-primary p-1'
+             
+              disabled={voucherURL=== null}
+              onClick={()=>ver(voucherURL)}
+              className={voucherURL === null? 'btn btn-secondary p-1 left':'btn btn-primary p-1 left'}
               data-toggle='modal'
               data-target='exampleModal'>
+       
               <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
             </button>
           </td>
@@ -151,18 +213,18 @@ export const Regitsers = ({ data, itemsPerPage }) => {
   return (
     <div className='content_base'>
       <Sidebar></Sidebar>
-      <div className='container contentwithoutsidebar'>
+      <div className=' contentwithoutsidebar'>
         <h1 className='mb-1'>
           Registros <FontAwesomeIcon icon={faFolder} />{' '}
         </h1>
-        <table className='table table-striped'>
+        <table className='table table-striped forceo'>
           <thead className='header_dark'>
             <tr>
               <th className='paddin_table' scope='col'>
-                Tipo Pago
+                T. Pago
               </th>
               <th className='paddin_table' scope='col'>
-                Fecha Registro
+                Registro
               </th>
               <th className='paddin_table' scope='col'>
                 Inscripciones
@@ -185,7 +247,6 @@ export const Regitsers = ({ data, itemsPerPage }) => {
         </table>
       </div>
       {renderPagination()}
-        
-    </div>
-  );
+    </div>
+  );
 };
