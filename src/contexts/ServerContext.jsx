b2a613d616137902036acca1fb1e49bbfbe099e3
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-06-07 00:53:54 -0500

    ci: :bug: remove github actions

--- src/contexts/ServerContext.jsx
+++ src/contexts/ServerContext.jsx
@@ -17,8 +17,8 @@ const getToken = () => {
 
 class ServerApi {
   constructor() {
-    this.baseUrl = 'http://138.68.78.23:7000';
-    // this.baseUrl = process.env.API_URL;
+    // this.baseUrl = 'http://138.68.78.23:7000';
+    this.baseUrl = import.meta.env.VITE_API;
   }
 
   getCourses = () => {
