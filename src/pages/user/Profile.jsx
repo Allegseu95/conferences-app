13be59fa7c8c178bb51ce019bf1d3a0fe5da32ad
Rommel-Palma <rommelpalma.3000@gmail.com>
2023-06-20 17:33:33 -0500

    fix: :bug: Agregacion de imagenes a registros
    Se sgregaron 3 imagenes que se descargaran junto con el registro

--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -59,19 +59,21 @@ export const Profile = () => {
     <div className='page-proflie'>
       <InfoProfile name={upperFirstWord(user?.name)} />
       <div className='row mt-2 justify-content-center'>
-        <div className='col-sm-10'>
+        <div className='col-sm-12'>
           <div className='alerta card m-3 mt-4'>
             <div className='card-header m-1 p-2'>Informacion Personal</div>
             <form onSubmit={(e) => e.preventDefault()}>
               <div className='card-body'>
-                <div className='row row-cols-1 row-cols-md-2'>
-                  <div className='col-md-2 col-12 p-1'>
+                <div className='row row-cols-1 row-cols-md-2 row-cols-lg-2 ow-cols-xl-2'>
+                  <div className='col-lg-4 col-xl-2 col-md-4 col-12 p-1'>
                     <b className='input-group-text p-1 fs-5'>
-                      <CgProfile className='fs-4 text-info m-1' />
+                      <div>
+                        <CgProfile className='fs-4 text-info m-1' />
+                      </div>
                       Nombre
                     </b>
                   </div>
-                  <div className='col-md-10 col-12 p-1'>
+                  <div className='col-lg-8 col-xl-10 col-md-8 col-12 p-1'>
                     <input
                       onChange={(e) => setEditedUser({ ...editedUser, name: e.target.value })}
                       value={upperFirstWord(editedUser?.name)}
@@ -82,13 +84,14 @@ export const Profile = () => {
                   </div>
                 </div>
                 <div className='row row-cols-1 row-cols-md-2'>
-                  <div className='col-md-2 col-12 p-1'>
+                  <div className='col-lg-4 col-xl-2 col-md-4 col-12 p-1'>
                     <b className='input-group-text p-1 fs-5'>
                       <RiProfileLine className='fs-4 text-info m-1' />
+                      <div></div>
                       Apellido
                     </b>
                   </div>
-                  <div className='col-md-10 col-12 p-1'>
+                  <div className='col-lg-8 col-xl-10 col-md-8 col-12 p-1'>
                     <input
                       onChange={(e) => setEditedUser({ ...editedUser, lastname: e.target.value })}
                       value={upperFirstWord(editedUser?.lastname)}
@@ -100,13 +103,15 @@ export const Profile = () => {
                 </div>
 
                 <div className='row row-cols-1 row-cols-md-2'>
-                  <div className='col-md-2 col-12 p-1'>
+                  <div className='col-lg-4 col-xl-2 col-md-4 col-12 p-1'>
                     <b className='input-group-text p-1 fs-5'>
-                      <MdLocationPin className='fs-4 text-success m-1' />
+                      <div>
+                        <MdLocationPin className='fs-4 text-success m-1' />
+                      </div>
                       Direccion
                     </b>
                   </div>
-                  <div className='col-md-10 col-12 p-1'>
+                  <div className='col-lg-8 col-xl-10 col-md-8 col-12 p-1'>
                     <input
                       onChange={(e) => setEditedUser({ ...editedUser, address: e.target.value })}
                       value={upperFirstWord(editedUser?.address)}
@@ -118,13 +123,15 @@ export const Profile = () => {
                 </div>
 
                 <div className='row row-cols-1 row-cols-md-2'>
-                  <div className='col-md-4 col-12 p-1'>
+                  <div className='col-lg-5 col-xl-3 col-md-6 col-12 p-1'>
                     <b className='input-group-text p-1 fs-5'>
-                      <CgOrganisation className='fs-4 text-secondary m-1' />
+                      <div>
+                        <CgOrganisation className='fs-4 text-secondary m-1' />
+                      </div>
                       Empresa/Institucion
                     </b>
                   </div>
-                  <div className='col-md-8 col-12 p-1'>
+                  <div className='col-lg-7 col-xl-9 col-md-6 col-12 p-1'>
                     <input
                       onChange={(e) => setEditedUser({ ...editedUser, company: e.target.value })}
                       value={editedUser?.company}
@@ -136,13 +143,15 @@ export const Profile = () => {
                 </div>
 
                 <div className='row row-cols-1 row-cols-md-2'>
-                  <div className='col-md-2 col-12 p-1'>
+                  <div className='col-lg-4 col-xl-2 col-md-4 col-12 p-1'>
                     <b className='input-group-text p-1 fs-5'>
-                      <BsPhoneFill className='fs-4 text-warning m-1' />
+                      <div>
+                        <BsPhoneFill className='fs-4 text-warning m-1' />
+                      </div>
                       Telefono
                     </b>
                   </div>
-                  <div className='col-md-10 col-12 p-1'>
+                  <div className='col-lg-8 col-xl-10 col-md-8 col-12 p-1'>
                     <input
                       onChange={(e) => setEditedUser({ ...editedUser, phone: e.target.value })}
                       value={editedUser?.phone}
@@ -154,13 +163,15 @@ export const Profile = () => {
                 </div>
 
                 <div className='row row-cols-1 row-cols-md-2'>
-                  <div className='col-md-2 col-12 p-1'>
+                  <div className='col-lg-4 col-xl-2 col-md-4 col-12 p-1'>
                     <b className='input-group-text p-1 fs-5'>
-                      <HiOutlineIdentification className='fs-4 text-primary m-1' />
+                      <div>
+                        <HiOutlineIdentification className='fs-4 text-primary m-1' />
+                      </div>
                       Cedula
                     </b>
                   </div>
-                  <div className='col-md-10 col-12 p-1'>
+                  <div className='col-lg-8 col-xl-10 col-md-8 col-12 p-1'>
                     <input
                       onChange={(e) => setEditedUser({ ...editedUser, cedula: e.target.value })}
                       value={editedUser?.cedula}
